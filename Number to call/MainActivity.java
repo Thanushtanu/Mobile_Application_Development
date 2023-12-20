f955f4a967f7edf21afb9023b62972ce636d8e97
THANUSH_TANU <105310864+Thanushtanu@users.noreply.github.com>
2023-12-20 15:15:43 +0530

    Create MainActivity.java

---
+++ Number to call/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.call;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button button;
+    EditText text;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        button = findViewById(R.id.button);
+        text = findViewById(R.id.editTextPhone);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = (text.getText().toString());
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" +s));
+                startActivity(intent);
+
+            }
+        });
+    }
+}
